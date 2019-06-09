2d53b2306dd40a2745afb47a8409369355ff5ccd
Ziya Mollamahmut <ziyadmm@gmail.com>
2019-06-09 18:04:38 +0300

    in progress: identity views localized resources

--- ExpressLocalizationSampleProject/Pages/Index.cshtml.cs
+++ ExpressLocalizationSampleProject/Pages/Index.cshtml.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using LazZiya.ExpressLocalization;
+using LazZiya.TagHelpers.Alerts;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ExpressLocalizationSampleProject.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly SharedCultureLocalizer _loc;
+
+        public IndexModel(SharedCultureLocalizer loc)
+        {
+            _loc = loc;
+        }
+
         public void OnGet()
         {
-
+            TempData.Primary(_loc.Text("click here").Value);
         }
     }
 }
